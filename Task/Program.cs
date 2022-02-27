8d6585a8119281289bd225e81dfe688716fc2cab
Denis Y <dyampolsky@yokohama-oht.com>
2022-02-27 20:19:13 +0200

    commit

---
+++ Task/Program.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AmazingConApp
+{
+    #region
+
+    using System;
+
+    using AmazingConApp.Properties;
+
+    #endregion
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+            if (string.IsNullOrWhiteSpace(Settings.Default.IsKnown))
+            {
+                Console.WriteLine(Settings.Default.GreetingsUnknow);
+
+                if (string.IsNullOrWhiteSpace(Settings.Default.Name))
+                {
+                    Console.WriteLine(Settings.Default.GetName);
+                    Settings.Default.Name = Console.ReadLine();
+                }
+
+                if (string.IsNullOrWhiteSpace(Settings.Default.Age))
+                {
+                    Console.WriteLine(Settings.Default.GetAge);
+                    Settings.Default.Age = Console.ReadLine();
+                }
+
+                if (string.IsNullOrWhiteSpace(Settings.Default.Occupation))
+                {
+                    Console.WriteLine(Settings.Default.GetOccupation);
+                    Settings.Default.Occupation = Console.ReadLine();
+                }
+
+                Settings.Default.IsKnown = "yes";
+                Settings.Default.Save();
+            }
+
+            Console.WriteLine($"{Settings.Default.Greetings} {Settings.Default.Name}!");
+
+            Console.WriteLine("press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+#region
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle("AmazingConApp")]
+[assembly: AssemblyDescription("This is my homework for lesson 8")]
+[assembly: AssemblyCompany("My Company LTD")]
+[assembly: AssemblyCopyright("Copyright © 2022 - My Company LTD. All rights reserved.")]
+[assembly: AssemblyTrademark("My Company LTD")]
+[assembly: AssemblyProduct("Amazing console application v.1")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: Guid("18007a54-1b8a-45d2-a2b7-ef152a471862")]
+
+[assembly: AssemblyVersion("1.2.3.4")]
+[assembly: AssemblyFileVersion("1.2.3.4")]
